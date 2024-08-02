02dcfea4366f6325065231a91e299d366c929e98
TanNguyen <minhtan86a@gmail.com>
2024-08-02 20:19:00 +0700

    427-428 community page, active link for route

--- components/images/image-slideshow.js
+++ components/images/image-slideshow.js
@@ -42,6 +42,7 @@ export default function ImageSlideshow() {
           src={image.image}
           className={index === currentImageIndex ? classes.active : ""}
           alt={image.alt}
+          priority
         />
       ))}
     </div>
