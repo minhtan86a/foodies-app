6ffd32ebeebffc3427bde6159090729452435da8
TanNguyen <minhtan86a@gmail.com>
2024-08-02 20:48:45 +0700

    429 Meals layout and image with unknown dimensions

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,15 +1,25 @@
 import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
 
 export default function MealsPage() {
   return (
     <>
-      <h1>Meals Page</h1>
-      <p>
-        <Link href="/meals/meal1">Meal 1</Link>
-      </p>
-      <p>
-        <Link href="/meals/meal2">Meal 2</Link>
-      </p>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, create{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
     </>
   );
 }
